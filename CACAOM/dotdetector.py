11979bdfa8a5d5c791af87b928d35cb167097df4
Theon767 <52738560+Theon767@users.noreply.github.com>
2022-07-04 12:50:52 +0200

    Add files via upload

--- CACAOM/dotdetector.py
+++ CACAOM/dotdetector.py
@@ -6,27 +6,34 @@ Library for detection of dots in images
 """
 
 import cv2
+import numpy as np
 
-def footdetector(path_read,filename,threshold):
-    #threshold=10
-    #path_read=r'C:\Users\m1380\CACAOM\Image'
-    file=path_read+filename
-    image = cv2.imread(file) #Detect the coordinates of foot
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    shifted=hsv[:,:,0]
-    shifted[shifted>threshold]=127
-    return shifted
 
 def dotdetector(path_read,filename,minArea):#Detect Blob features
     #path_read=r'C:\Users\m1380\CACAOM\Image'
     #defalut minArea=70
     file=path_read+filename
     image = cv2.imread(file)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)   
     params = cv2.SimpleBlobDetector_Params()
+    
+    #change thresholds
+#   params.minThreshold = 10;
+#    params.maxThreshold = 200;
+    
+    #filter by area
     params.filterByArea = True   #setting up Blob detector
     params.minArea = minArea
+    
+    # Filter by Circularity
+    params.filterByCircularity = True
+    params.minCircularity = 0.3
+   
+
+    # Filter by Inertia
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.2
+
     detector = cv2.SimpleBlobDetector_create(params)
     keypoints = detector.detect(image)
     return keypoints
@@ -34,25 +41,7 @@ def dotdetector(path_read,filename,minArea):#Detect Blob features
     #blobs = cv2.drawKeypoints(image, keypoints, blank, (0, 0, 255),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 def crosscompare(contour,keypoints):
-    '''    N=len(keypoints) #used to ensure that Blob features lie on foot
-    print(N)
-    new_keypoints=keypoints
-    for i in range(N):
-        #x=list(range(round(keypoints[i].pt[0])-20,round(keypoints[i].pt[0])+20)) 
-        #y=list(range(round(keypoints[i].pt[1])-20,round(keypoints[i].pt[1])+20))
-        #print(i)
-        (x,y) = keypoints[i].pt
-        #print(keypoints[0].pt)
-        #print(keypoints[1].pt)
-        #print(keypoints[2].pt)
-        #print(keypoints[3].pt)
-        a=foot_image[round(x)-size:round(x)+size#form a square area around Blob features
-                      ,round(y)-size:round(y)+size]
-        if (a==127).any():
-            new_keypoints[i]=0
-            #print('yes')
-            #print(keypoints)
-    new_keypoints= [i for i in new_keypoints if i != 0]'''
+
     N=len(keypoints)
     new_keypoints=keypoints
     for i in range(N):
@@ -62,5 +51,18 @@ def crosscompare(contour,keypoints):
             new_keypoints[i]=0
     new_keypoints=[i for i in new_keypoints if i != 0]
     return new_keypoints
+
+def select (keypoints):
+    N=len(keypoints)
+    position=[]
+    new_keypoints=keypoints
+
+    for i in range(N):
+        (x,y) = keypoints[i].pt
+        position.append((x,y))
+    if N>6:
+        position=sorted(position,key=lambda x:x[1])
+        position=np.delete(position,obj=-1,axis=0)
+    return position
             
             
--- CACAOM/main.py
+++ CACAOM/main.py
@@ -13,18 +13,15 @@ from footdetector import *
     
 path_read=r'C:\Users\m1380\CACAOM\Video'
 path_write=r'C:\Users\m1380\CACAOM\Image'
-filename='\\frames_280.jpg'
-#video2image(path_read,path_write,'\\IMG_3998.MOV',10)# transform video to picture
+filename='\\frames_210.jpg'
+#video2image(path_read,path_write,'\\IMG_3997.MOV',10)# transform video to picture
 file=path_write+filename#load the file
 image=cv2.imread(file)
-
-
 keypoints=dotdetector(path_write,filename,minArea=60)#using blob to extract feature points,minArea suggest the size 
                                                                         #of the blob features
 
-max_contour=footdetector(path_write,filename,ksize1=(9,9),ksize2=(3,3))#using Canny&Contour to find the contour of 
+max_contour=footdetector(path_write,filename,ksize1=(11,11),ksize2=(3,3))#using Canny&Contour to find the contour of 
                                                                             #feet 
-
 new_keypoints=crosscompare(max_contour,keypoints)  # eliminate feature points which are not on feet
 #new_keypoints=keypoints
 #detect foot and Blob features and ensure that features are on foot
@@ -33,6 +30,10 @@ new_keypoints=crosscompare(max_contour,keypoints)  # eliminate feature points wh
 blank = np.zeros((1, 1))
 blobs = cv2.drawKeypoints(image, new_keypoints, blank, (0, 0, 255),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 cv2.drawContours(image,max_contour, -1, (0,255,0), 3) #Draw biggest contour
+
+
+position=select(new_keypoints)
+print(position)
 
 cv2.imshow('blobs2',image)
 cv2.waitKey()
